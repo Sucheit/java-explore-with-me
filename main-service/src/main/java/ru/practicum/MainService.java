3d07592d979b92868f9e21b8cbeeba3e06897cd3
Arthur Inzhilov <artur-inzhilov@mail.ru>
2023-07-28 23:22:04 +0500

    Этап 2 Основной сервис

--- main-service/src/main/java/ru/practicum/MainService.java
+++ main-service/src/main/java/ru/practicum/MainService.java
@@ -3,7 +3,7 @@ package ru.practicum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"ru.practicum.main-service", "ru.practicum.statistics.statistics-client"})
+@SpringBootApplication(scanBasePackages = {"ru.practicum.main-service", "ru.practicum.statistics-client"})
 public class MainService {
     public static void main(String[] args) {
         SpringApplication.run(MainService.class, args);
