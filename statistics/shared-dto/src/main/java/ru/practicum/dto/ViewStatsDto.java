10d093c3abc6b0ec833c612aecdecba161726960
Arthur Inzhilov <artur-inzhilov@mail.ru>
2023-07-29 14:06:23 +0500

    Этап 2 Основной сервис

--- statistics/shared-dto/src/main/java/ru/practicum/dto/ViewStatsDto.java
+++ statistics/shared-dto/src/main/java/ru/practicum/dto/ViewStatsDto.java
@@ -1,10 +1,12 @@
 package ru.practicum.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Value;
 
 @Value
 @Builder
+@AllArgsConstructor
 public class ViewStatsDto {
 
     String app;
