f54dde70fc542e31b1c21e7ff75016de0231b6c1
Arthur Inzhilov <artur-inzhilov@mail.ru>
2023-07-30 13:18:24 +0500

    Этап 2 Основной сервис

--- ewm-main/src/main/java/ru/practicum/mainservice/ExploreWithMe.java
+++ ewm-main/src/main/java/ru/practicum/mainservice/ExploreWithMe.java
@@ -3,7 +3,7 @@ package ru.practicum.mainservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"ru.practicum.main-service", "ru.practicum.statistics-client"})
+@SpringBootApplication(scanBasePackages = {"ru.practicum.mainservice","ru.practicum.statsclient"})
 public class ExploreWithMe {
     public static void main(String[] args) {
         SpringApplication.run(ExploreWithMe.class, args);
---
+++ ewm-main/src/main/java/ru/practicum/mainservice/TestController.java
@@ -0,0 +1,15 @@
+package ru.practicum.mainservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/test")
+public class TestController {
+
+    @GetMapping
+    public String test() {
+        return "TEST";
+    }
+}
